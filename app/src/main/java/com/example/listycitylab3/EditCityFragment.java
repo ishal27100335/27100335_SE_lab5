0265faa29e969e59652a22cf02e13d0732b3ba9b
ishal27100335 <27100335@lums.edu.pk>
2026-02-08 18:51:25 +0500

    lab 3

---
+++ app/src/main/java/com/example/listycitylab3/EditCityFragment.java
@@ -0,0 +1,64 @@
+package com.example.listycitylab3;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class EditCityFragment extends DialogFragment {
+
+    interface EditCityDialogListener {
+        void editCity(City city);
+    }
+
+    private EditCityDialogListener listener;
+    private City city;
+
+    public EditCityFragment(City city) {
+        this.city = city;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof EditCityDialogListener) {
+            listener = (EditCityDialogListener) context;
+        } else {
+            throw new RuntimeException(context + " must implement EditCityDialogListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        View view = LayoutInflater.from(getContext())
+                .inflate(R.layout.fragment_add_city, null);
+
+        EditText editCityName = view.findViewById(R.id.edit_text_city_text);
+        EditText editProvinceName = view.findViewById(R.id.edit_text_province_text);
+
+        // 🔽 Pre-fill existing values
+        editCityName.setText(city.getName());
+        editProvinceName.setText(city.getProvince());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder
+                .setView(view)
+                .setTitle("Edit City")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Save", (dialog, which) -> {
+                    city.setName(editCityName.getText().toString());
+                    city.setProvince(editProvinceName.getText().toString());
+                    listener.editCity(city);
+                })
+                .create();
+    }
+}
